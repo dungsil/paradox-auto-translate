d93b63cbcd0fd2186ad64d19140197d701957ccd
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-12-20 21:00:24 +0900

    chore: 프롬프트 개선

--- scripts/utils/constants.ts
+++ scripts/utils/constants.ts
@@ -23,25 +23,27 @@ export const PARADOX_PARSE_REGEX = /(.*:)(\d*)( *)(".*")(?:#(.*))?/
 
 // 프롬프트
 export const PROMPT = `
-Translate and output the value entered by the user to Korean
-
-Please consider the following when responding:
- - NOTHING should be responded to except the translated text.
- - Values wrapped in '$', '£' characters mean that they are variables, so don't translate them.
-   Example: '$k_france$' should remain as '$k_france$'
- - Sentences surrounded by '#' characters are syntax for formatting fonts, such as '#F blar blar#', '#bold ACCENT MESSAGE#!', etc. Do not translate the text you are formatting
-   Example: 'The #bold King#' should be translated as '#bold 왕#'
- - Characters wrapped in square brackets are also variables, so dont' translate them.
-   Example: '[culture|E] should be translated as '[culture|E]', [GetGeographicalRegion('world_europe_west_iberia').GetName] should be translated as [GetGeographicalRegion('world_europe_west_iberia').GetName]
- - Do not translate the content of the comment, but print it out verbatim
+## Instructions:
+1. NOTHING should be responded to except the translated text.
+2. Values wrapped in '$', '£', '@' characters mean that they are variables, so don't translate them.
+    - '$k_france$ → $k_france$
+    - £k_france£ → £k_france£
+    - @gold_icon@ → @gold_icon@
+3. Sentences surrounded by '#' characters are syntax for formatting fonts, such as '#F blar blar#', '#bold ACCENT MESSAGE#!', etc. Do not translate the text you are formatting
+    - #F blar blar# → #F blar blar#
+    - #bold ACCENT MESSAGE#! → #bold ACCENT MESSAGE#!
+    - #italic ACCENT MESSAGE# → #italic ACCENT MESSAGE#
+4. Characters wrapped in square brackets are also variables, so dont' translate them.
+    - [culture|E] → [culture|E]
+    - [GetGeographicalRegion('world_europe_west_iberia').GetName] → [GetGeographicalRegion('world_europe_west_iberia').GetName]
 `
 
 // CK3 프롬프트
 export const CK3_PROMPT = `
-You are a text file translator for a wargame set in the Middle Ages.
+You are a mod translator for the medieval strategy game “Crusader Kings III”. Please translate the provided text into Korean.
 
 ${PROMPT}
 
-Below is the translation memory, so refer to it as you translate:
+# Translation memory:
 ${Object.keys(dictionary).map((key) => ` - ${key}: ${dictionary[key]}`).join('\n')}
 `
