a5485fd439a35810c7212c3161458cdb56616225
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-12-25 17:52:09 +0900

    fix: retry 기능이 정상적으로 작동안하는 부분 개선

--- scripts/utils/ai.ts
+++ scripts/utils/ai.ts
@@ -1,7 +1,5 @@
 import { type GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai'
 import dotenv from 'dotenv'
-import { delay } from './delay'
-import { log } from './logger'
 import { Ck3_SYSTEM_PROMPT } from './prompts'
 import { addQueue } from './queue'
 
@@ -31,29 +29,22 @@ const gemini15flash = ai.getGenerativeModel({
 export async function translateAI (text: string) {
   return new Promise<string>((resolve, reject) => {
     try {
-      addQueue(
-        async () => {
-
-          let translatedText: string
-
-          try {
-            translatedText = await translateAIByModel(gemini20, text)
-          } catch (e) {
-            log.debug(`'${gemini20.model}' 모델 요청에 실패했습니다. 2초 후 '${gemini15flash.model}' 모델로 재시도합니다.`)
-            await delay(2000) // wait 2 seconds
-            translatedText = await translateAIByModel(gemini15flash, text)
-          }
-
-          resolve(translatedText)
-        },
-      )
-    } catch (error) {
-      reject(error)
+      return translateAIByModel(resolve, gemini20, text)
+    } catch (e) {
+      try {
+        return translateAIByModel(resolve, gemini15flash, text)
+      } catch (ee) {
+        reject(ee)
+      }
     }
   })
 }
 
-async function translateAIByModel (model: GenerativeModel, text: string): Promise<string> {
-  const { response } = await model.generateContent(text)
-  return response.text().trim().replaceAll(/\n/g, '\\n')
+async function translateAIByModel (resolve: (value: string | PromiseLike<string>) => void, model: GenerativeModel, text: string): Promise<void> {
+  return addQueue(
+    async () => {
+      const { response } = await model.generateContent(text)
+      resolve(response.text().trim().replaceAll(/\n/g, '\\n'))
+    },
+  )
 }
