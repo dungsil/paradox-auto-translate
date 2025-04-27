02e2c42e73cb9fc5244ed886cd7fcc435edf9586
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2025-04-28 00:16:04 +0900

    refactor: 잘못된 번역 결과 재출 시 다시 번역을 요청하도록 개선

--- scripts/utils/translate.ts
+++ scripts/utils/translate.ts
@@ -1,11 +1,17 @@
 import { translateAI } from './ai'
 import { getCache, hasCache, setCache } from './cache'
 import { getDictionary, hasDictionary } from './dictionary'
+import { log } from './logger.js'
 
 const INNER_FUNCTION_REGEX = /^\[([\w.]+(?:\|[\w.]+)?|[\w.]+\.[\w.]+\(['\w.]+'\))]$/
 const VARIABLE_REGEX = /^\$[a-zA-Z0-9_\-.]+\$$/
 
-export async function translate (text: string): Promise<string> {
+export async function translate (text: string, retry: number = 0): Promise<string> {
+
+  if (retry > 5) {
+    log.error('재시도 횟수 초과로 종료, 대상 텍스트: "', text, '"')
+    process.exit(-1)
+  }
 
   // 잘못된 형식인 경우 빈 텍스트 반환
   if (!text) {
@@ -36,7 +42,15 @@ export async function translate (text: string): Promise<string> {
 
   // 실제 AI 번역 요청
   const translatedText = await translateAI(text)
-  await setCache(text, translatedText)
 
+  // 잘못된 결과 재 번역 시도
+  if (
+    translatedText.toLowerCase().includes('language model') ||
+    translatedText.match(/\[[가-힣]/g)
+  ) {
+    return await translateAI(text, retry++)
+  }
+
+  await setCache(text, translatedText)
   return translatedText
 }
