ad97ce22a791799be963e44382d9a1f40fb34d08
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2025-06-13 22:44:40 +0900

    refactor: 스크립트 정리

--- scripts/ck3.ts
+++ scripts/ck3.ts
@@ -2,6 +2,7 @@ import process from 'node:process'
 import { readdir } from 'node:fs/promises'
 import { join } from 'pathe'
 import { processModTranslations } from './factory/translate'
+import { invalidateDictionaryTranslations } from './utils/dictionary-invalidator'
 import { log } from './utils/logger'
 
 async function main () {
@@ -9,23 +10,38 @@ async function main () {
     const ck3Dir = join(import.meta.dirname, '..', 'ck3')
     const mods = await readdir(ck3Dir)
     const onlyHash = process.argv?.[2] === 'onlyHash'
+    const updateDict = process.argv?.[2] === 'updateDict'
 
-    log.box(
-      `
-      CK3 번역 스크립트 구동
-      - 번역 대상 경로: ${ck3Dir}
-      - 번역 대상 모드 (${mods.length}개): ${mods}
-      `,
-    )
+    if (updateDict) {
+      log.box(
+        `
+        CK3 단어사전 기반 번역 무효화
+        - 대상 경로: ${ck3Dir}
+        - 대상 모드 (${mods.length}개): ${mods}
+        `,
+      )
+      
+      await invalidateDictionaryTranslations('ck3', ck3Dir)
+      
+      log.success(`단어사전 기반 번역 무효화 완료!`)
+    } else {
+      log.box(
+        `
+        CK3 번역 스크립트 구동
+        - 번역 대상 경로: ${ck3Dir}
+        - 번역 대상 모드 (${mods.length}개): ${mods}
+        `,
+      )
 
-    await processModTranslations({
-      rootDir: ck3Dir,
-      mods,
-      gameType: 'ck3',
-      onlyHash
-    })
+      await processModTranslations({
+        rootDir: ck3Dir,
+        mods,
+        gameType: 'ck3',
+        onlyHash
+      })
 
-    log.success(`번역 완료! 스크립트를 종료합니다. (처리된 모드: ${mods})`)
+      log.success(`번역 완료! 스크립트를 종료합니다. (처리된 모드: ${mods})`)
+    }
   } catch (error) {
     log.error('번역 도중 오류가 발생하였습니다.', error)
     process.exit(1)
--- scripts/stellaris.ts
+++ scripts/stellaris.ts
@@ -2,6 +2,7 @@ import process from 'node:process'
 import { readdir } from 'node:fs/promises'
 import { join } from 'pathe'
 import { processModTranslations } from './factory/translate'
+import { invalidateDictionaryTranslations } from './utils/dictionary-invalidator'
 import { log } from './utils/logger'
 
 async function main () {
@@ -9,23 +10,38 @@ async function main () {
     const stellarisDir = join(import.meta.dirname, '..', 'stellaris')
     const mods = await readdir(stellarisDir)
     const onlyHash = process.argv?.[2] === 'onlyHash'
+    const updateDict = process.argv?.[2] === 'updateDict'
 
-    log.box(
-      `
-      Stellaris 번역 스크립트 구동
-      - 번역 대상 경로: ${stellarisDir}
-      - 번역 대상 모드 (${mods.length}개): ${mods}
-      `,
-    )
+    if (updateDict) {
+      log.box(
+        `
+        Stellaris 단어사전 기반 번역 무효화
+        - 대상 경로: ${stellarisDir}
+        - 대상 모드 (${mods.length}개): ${mods}
+        `,
+      )
+      
+      await invalidateDictionaryTranslations('stellaris', stellarisDir)
+      
+      log.success(`단어사전 기반 번역 무효화 완료!`)
+    } else {
+      log.box(
+        `
+        Stellaris 번역 스크립트 구동
+        - 번역 대상 경로: ${stellarisDir}
+        - 번역 대상 모드 (${mods.length}개): ${mods}
+        `,
+      )
 
-    await processModTranslations({
-      rootDir: stellarisDir,
-      mods,
-      gameType: 'stellaris',
-      onlyHash
-    })
+      await processModTranslations({
+        rootDir: stellarisDir,
+        mods,
+        gameType: 'stellaris',
+        onlyHash
+      })
 
-    log.success(`번역 완료! 스크립트를 종료합니다. (처리된 모드: ${mods})`)
+      log.success(`번역 완료! 스크립트를 종료합니다. (처리된 모드: ${mods})`)
+    }
   } catch (error) {
     log.error('번역 도중 오류가 발생하였습니다.', error)
     process.exit(1)
